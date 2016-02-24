f59741b9348922319ce07637d761da6a2a25a8c2
Alexey Melezhik <melezhik@gmail.com>
2016-02-24 13:39:28 +0300

    new version

--- lib/swat/dancer.pm
+++ lib/swat/dancer.pm
@@ -7,15 +7,19 @@ sub start_dancer_app {
 
     my $project_root_dir = project_root_dir();
 
+    return if -f test_root_dir()."/run.ok";
+
     system("cd $project_root_dir && ".
     "export project_root_dir=$project_root_dir &&".
     "nohup carton exec nohup plackup ".
-    "--host 0.0.0.0 --port 5000 ".
+    "--host 0.0.0.0 --port $ENV{port} ".
     "--access-log ".test_root_dir()."/access.log ".
     "--error-log ".test_root_dir()."/error.log ".
-    'app.pl 1>/dev/null 2>/dev/null & echo $! > /tmp/app.pid');
+    'app.psgi 1>/dev/null 2>/dev/null & echo $! > /tmp/app.pid ').
+    "&& touch ".test_root_dir()."/run.ok";
 
     my $pid = get_app_pid();
+
     ok($pid,"dancer is running . pid: $pid");
 
 }
